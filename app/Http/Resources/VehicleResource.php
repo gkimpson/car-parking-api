2e87cc26ade626b56c1f1645c7fce05dd809b6d8
Gavin Kimpson <gkimpson@gmail.com>
2023-01-18 18:04:40 +0000

    resources setup

---
+++ app/Http/Resources/VehicleResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class VehicleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        // return parent::toArray($request);
+
+        return [
+            'id' => $this->id,
+            'reg_number' => $this->reg_number,
+        ];
+    }
+}
