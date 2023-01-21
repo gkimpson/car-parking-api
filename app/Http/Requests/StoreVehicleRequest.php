037040c9af94162e2e65f614661cb8f869a59cbb
Gavin Kimpson <gkimpson@gmail.com>
2023-01-21 14:16:08 +0000

    extra validation

--- app/Http/Requests/StoreVehicleRequest.php
+++ app/Http/Requests/StoreVehicleRequest.php
@@ -24,7 +24,7 @@ class StoreVehicleRequest extends FormRequest
     public function rules()
     {
         return [
-            'reg_number' => 'required',
+            'reg_number' => 'required|min:5|max:16',
         ];
     }
 }
